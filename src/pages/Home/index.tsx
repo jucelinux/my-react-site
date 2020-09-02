2996009bba238070c3b73659cce70d28d8bba9d1
jucelinux <jucelino.qjr@gmail.com>
2020-09-01 22:04:30 -0300

    Creating project scaffold

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles'
+
+const Home: React.FC = () => {
+  return (
+      <S.Container>
+          <h1>React nem é lento</h1>
+      </S.Container>
+  );
+}
+
+export default Home;
---
+++ src/styles/global.ts
@@ -0,0 +1,42 @@
+import {createGlobalStyle} from 'styled-components'
+
+export default createGlobalStyle`
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
+html {
+  font-size: 62.5%; /* 1rem = 10px */
+}
+html,
+body,
+#root {
+  min-width: 100%;
+  height: 100%;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -webkit-tap-highlight-color: transparent;
+  -webkit-overflow-scrolling: touch;
+}
+body,
+input,
+button {
+  font: 1.6rem sans-serif;
+}
+h1, h2, h3, h4, h5, h6, strong {
+  font-weight: 700;
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
+}
+li {
+  list-style: none;
+}
+button {
+  cursor: pointer;
+}
+`
